fc978c542d8b9a2296404c68292deb7034bc47ec
Serhat <serhat@Serhat-MacBook-Air.local>
2022-11-23 15:28:14 +0300

    some visual updates

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-
+import AddImage from "../img/addImage.svg"
+import Attach from "../img/attach.svg"
 const Input = () => {
   return (
     <div className="input">
       <input type="text" placeholder="Type Something..." />
-      <img src="" alt="" />
-      <input type="file" style={{ display: "none" }} id="file" />
-      <label htmlFor="file"></label>
-      <button>Send</button>
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+        <img src={AddImage} alt="" /></label>
+        <button>Send</button>
+      </div>
     </div>
   );
 };
